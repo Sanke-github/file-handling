858ac1aad61d1e2689535d411cc2e7b00283527c
Sankeerth <86974454+Sanke-github@users.noreply.github.com>
2023-01-27 11:21:47 +0530

    Update program1.py

--- csv-files/program1.py
+++ csv-files/program1.py
@@ -1,11 +1,18 @@
+'''write a menu driven program that has the following functions:
+create() - add data to student.csv
+search() search records whose fees is above 20k and below 40k and add them to a new file final.csv
+display() - to dispay the old and new records '''
+
+
 import csv
 def create():
+    # open file to write in
     file = open('Student.csv','w')
     writer = csv.writer(file) 
     writer.writerow(['Rollno','Name','Subjects','fees'])
     ans = 'y'
     count = 0
-
+    # loop to add multiple records
     while ans == 'y':
         Rollno = int(input('enter your roll number : '))
         Name = input('enter your name : ')
@@ -15,17 +22,24 @@ def create():
         writer.writerow(lst)
         count += 1
         print(f'\n{count} records added')
+        # check if more records need to be added
 
         ans = input('\nmore records? [y/n] : ')
 def Search():
+    # open the file in read mode
     file = open('student.csv','r',newline = '\n')
     final_file = open('final.csv','w',newline = '\n')
+    
+    # open the file to write the filtered records
     writer = csv.writer(final_file,delimiter = ',')
+    
+    # write the header row
     writer.writerow(['Rollno','Name','Subjects','fees'])
     reader = csv.reader(file)
     count = 0
     count_new = 0
     sum_    = 0
+    # skipping the header row
     next(reader,None)
     for row in reader:
         count += 1
@@ -42,15 +56,17 @@ def Search():
 def display_old():
     file = open('student.csv','r',newline = '\n')
     reader = csv.reader(file)
-    for i in reader:
-        print(i)
+    
+    # loop through the records and display
+    for records in reader:
+        print(records)
     file.close()
     
 def display_new():
     file = open('final.csv','r',newline = '\n')
     reader = csv.reader(file)
-    for i in reader:
-        print(i)
+    for records in reader:
+        print(records)
         
 while True:
     print('\n\t MENU BAR\n')
