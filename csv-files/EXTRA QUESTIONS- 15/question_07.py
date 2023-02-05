ceb9ed4b4c42fc10f0e064610a8386f63a38e490
Sankeerth <86974454+Sanke-github@users.noreply.github.com>
2023-02-05 18:43:26 +0530

    Add files via upload

---
+++ csv-files/EXTRA QUESTIONS- 15/question_07.py
@@ -0,0 +1,46 @@
+'''
+Q7. Write a Program in Python that defines and calls the following user defined functions:
+(i) add() – To accept and add data of an employee to a CSV file ‘furdata.csv’. Each record consists of a list with field
+elements as fid, fname and fprice to store furniture id, furniture name and furniture price respectively.
+(ii) search()- To display the records of the furniture whose price is less than 50000.
+'''
+import csv 
+
+def add():
+    file = open('furdata.csv','w')
+    writer = csv.writer(file)
+    writer.writerow(['f_id','f_name','fprice'])
+    ans = 'y'
+    list = []
+    while ans == 'y':
+        eid = int(input('enter furniture id: '))
+        name = input('enter furniture name: ')
+        price = int(input('enter furniture price: '))
+        ans = input('more records? ')
+        lst = ([eid,name,price])
+        list.append(lst)
+    writer.writerows(list)
+    file.close()
+
+def search():
+    file = open('furdata.csv','r',newline = '\n')
+    reader = csv.reader(file)
+    next(reader)
+    for i in reader:
+        if int(i[2]) <= 50000:
+            print(i)
+    file.close()
+
+def start():
+    print('1.add records')
+    print('2.search')
+    print('3.exit')
+    ch = int(input('enter choice: '))
+    if ch == 1:
+        add()
+    elif ch == 2:
+        search()
+    else:
+        'record not found'
+    start()
+start()
